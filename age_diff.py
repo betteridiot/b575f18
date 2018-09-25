3edd16758db7b265ff3e00f773b64da6db3d1f4c
betteridiot <mdsherman@betteridiot.tech>
2018-09-25 06:53:40 -0400

    Getting setup for class

---
+++ age_diff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Module-level docstring
+"""
+
+def do_something():
+    """Function-level docstring
+    """
+
+
+def main(*args, **kwargs):
+    do_something()
+
+
+if __name__ == "__main__":
+    first_person_age = sys.argv[1]
+    second_person_age = sys.argv[2]
