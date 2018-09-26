e2f69c3eba4b2e4254083305390f0bd492e99cc6
betteridiot <mdsherman@betteridiot.tech>
2018-09-26 07:14:11 -0400

    Rearranged the lesson

---
+++ name_main_demo.py
@@ -0,0 +1,16 @@
+# Top-level commands
+print('Top-level loaded')
+
+# Encapsulating operations within functions
+def foo():
+    print('Marcus was here')
+    return None
+
+# What happens from the command line
+def main():
+    print('Command line curator')
+    return None
+
+if __name__ == '__main__':
+    print('CLI')
+    main()
