51df040e82bb564829894c029e544f6e2365df02
Marcus D Sherman <betteridiot@users.noreply.github.com>
2018-10-17 10:01:36 -0400

    Delete assignment.py

--- assignment.py
+++
@@ -1,9 +0,0 @@
-import sys
-import st
-
-fasta_filename = sys.argv[1]
-
-with open(fasta_filename) as fasta_file:
-    for header, seq  in st.get_next_fasta(fasta_file):
-        print(header)
-        print(len(seq))
